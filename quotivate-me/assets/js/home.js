1c3d54f341597032e74d69f59809e6a7ab59d89e
ChaMow <jchandramowli15039@it.ssn.edu.in>
2018-02-19 21:18:07 +0530

    Add Random Quotes for Popup with Gradient Style

--- quotivate-me/assets/js/home.js
+++ quotivate-me/assets/js/home.js
@@ -1,4 +1,7 @@
 //Event Listeners
+document.addEventListener('DOMContentLoaded', getRandomFile());
+document.addEventListener('DOMContentLoaded', backgroundGradient());
+
 function readTextFile(file, callback) {
     var rawFile = new XMLHttpRequest();
     rawFile.overrideMimeType("application/json");
@@ -11,10 +14,98 @@ function readTextFile(file, callback) {
     rawFile.send(null);
 }
 
-//usage:
+function getRandomQuote()
+{
+    return Math.floor(Math.random() * 20) + 1;
+}
 
-readTextFile("/data/001.json", function(text){
-    var data = JSON.parse(text);
-    console.log(data);
-    document.getElementById("quote").innerText = data["data"][0]["quote"];
-});
+function readFromFile(path, fileNumber)
+{
+    readTextFile(path + fileNumber, function(text){
+        var data = JSON.parse(text);
+        var randomQuoteNumber = getRandomQuote();
+        console.log(randomQuoteNumber);
+        document.getElementById("quote").innerText = data["data"][randomQuoteNumber]["quote"];
+        document.getElementById("author").innerText = "- " + data["data"][randomQuoteNumber]["author"];
+    });
+}
+
+//read from random file
+function getRandomFile()
+{
+    var path = "/data/";
+    var fileNumber = (Math.floor(Math.random() * 141)).toString();
+    for(var i = 0; i < (3 - fileNumber.length); i++)
+    {
+        fileNumber = '0' + fileNumber;
+    }
+    fileNumber += ".json";
+    console.log(fileNumber);
+    readFromFile(path, fileNumber);
+}
+
+function backgroundGradient()
+{
+
+    var colors = new Array(
+        [62,35,255],
+        [60,255,60],
+        [255,35,98],
+        [45,175,230],
+        [255,0,255],
+        [255,128,0]);
+
+    var step = 0;
+//color table indices for:
+// current color left
+// next color left
+// current color right
+// next color right
+    var colorIndices = [0,1,2,3];
+
+//transition speed
+    var gradientSpeed = 0.002;
+
+    function updateGradient()
+    {
+
+        if ( $===undefined ) return;
+
+        var c0_0 = colors[colorIndices[0]];
+        var c0_1 = colors[colorIndices[1]];
+        var c1_0 = colors[colorIndices[2]];
+        var c1_1 = colors[colorIndices[3]];
+
+        var istep = 1 - step;
+        var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+        var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+        var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+        var color1 = "rgb("+r1+","+g1+","+b1+")";
+
+        var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+        var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+        var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+        var color2 = "rgb("+r2+","+g2+","+b2+")";
+
+        $('#gradient').css({
+            background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
+            background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
+
+        step += gradientSpeed;
+        if ( step >= 1 )
+        {
+            step %= 1;
+            colorIndices[0] = colorIndices[1];
+            colorIndices[2] = colorIndices[3];
+
+            //pick two new target color indices
+            //do not pick the same as the current one
+            colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+            colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+
+        }
+    }
+
+    setInterval(updateGradient,10);
+
+}
