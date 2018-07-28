99c1029ee0f6e562e1716858f063f02f999b905e
OEM Configuration (temporary user) <jchandramowli15039@it.ssn.edu.in>
2018-07-28 22:05:17 +0530

    Complete Settings Page

--- quotivate-me/assets/js/options.js
+++ quotivate-me/assets/js/options.js
@@ -9,39 +9,32 @@ function loadSettings() {
     chrome.storage.local.get(["basicMode"], function(result) {
         if(result.basicMode >= 20) {
             basicMode.checked = true;
-            basicMode();
-            document.getElementById("basic-mode-duration").innerText = result.basicMode;
+            console.log(result.basicMode);
+            document.getElementById("basic-mode-duration").value = result.basicMode;
         }
         else {
             basicMode.checked = false;
+            $("#basic-mode").hide();
         }
     });
     chrome.storage.local.get(["proMode"], function(result) {
         if(result.proMode >= 20) {
             proMode.checked = true;
-            proMode();
-            document.getElementById("pro-mode-duration").innerText = result.proMode;
+            document.getElementById("pro-mode-duration").value = result.proMode;
         }
         else {
             proMode.checked = false;
+            $("#pro-mode").hide();
         }
     });
 }
 
 function basicMode() {
-    var basicModeButton = document.getElementById("basic-mode-box");
-    var basicMode = document.getElementById("basic-mode");
-    basicMode.innerHTML = basicModeButton.checked ?
-        "Show me quotes every <input id='basic-mode-duration' type='number'></input> minutes" :
-        "";
+    document.getElementById("basic-mode-box").checked ? $("#basic-mode").show() : $("#basic-mode").hide();
 }
 
 function proMode() {
-    var proModeButton = document.getElementById("pro-mode-box");
-    var proMode = document.getElementById("pro-mode");
-    proMode.innerHTML = proModeButton.checked ?
-        "Show me videos every <input id='pro-mode-duration' type='number'></input> minutes" :
-        "";
+    document.getElementById("pro-mode-box").checked ? $("#pro-mode").show() : $("#pro-mode").hide();
 }
 
 function saveSettings() {
@@ -58,13 +51,9 @@ function saveSettings() {
         settingsStatus.innerText = "Pro mode duration should be minimum 40 minutes.";
         return;
     }
-    if(basicModeStatus) {
-        chrome.storage.local.set({"basicMode": basicModeDuration}, function() {
-        });
-    }
-    if(proModeStatus) {
-        chrome.storage.local.set({"proMode": proModeDuration}, function() {
-        });
-    }
+    chrome.storage.local.set({"basicMode": basicModeDuration}, function() {
+    });
+    chrome.storage.local.set({"proMode": proModeDuration}, function() {
+    });
     settingsStatus.innerText = "Save successful.";
 }
